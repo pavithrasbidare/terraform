4e0e7e8c374f2171c3fd586b53a470556f2ecc7c
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-21 14:40:46 +0530

    Update security.tf

--- Usecase_1/Infra/security.tf
+++ Usecase_1/Infra/security.tf
@@ -55,3 +55,53 @@ resource "aws_security_group" "db_sg" {
     Name = "db-sg"
   }
 }
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+
+# Create NAT Gateway
+resource "aws_eip" "nat_eip" {
+  vpc = true
+  tags = {
+    Name = "nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public1.id
+   tags = {
+    Name = "main-nat"
+  }
+}
+
+# Create Route Tables
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+   tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+   tags = {
+    Name = "private-route-table"
+  }
+}
