eac24121e94727ec201ee3890cfa510b29365ee8
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-21 14:39:51 +0530

    Update subnets.tf

--- Usecase_1/Infra/subnets.tf
+++ Usecase_1/Infra/subnets.tf
@@ -33,3 +33,24 @@ resource "aws_subnet" "private2" {
     Name = "private-subnet-2"
   }
 }
+
+# Associate Route Tables with Subnets
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private2" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.private.id
+}
