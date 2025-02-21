98541f29b6b2e53f45a7f9ba1c79207024e019da
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-21 13:16:31 +0530

    Update backend.tf

--- Usecase_1/Infra/backend.tf
+++ Usecase_1/Infra/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket         = "nsh-usecase1"
-    key            = "env:/dev/terraform.tfstate"
+    key            = "env:/dev/uc1/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "terraform-locks"
   }
